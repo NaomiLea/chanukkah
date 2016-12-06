3cadbad9f63d77b015c0181c7fe1e9514260423a
Naomi Kudren <N.Kudren@amaze.com>
2016-12-06 10:53:04 +0000

    adding drag and drop

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
-  $(function() {
-      $("#lighting").draggable({ revert: "valid" });
+$(function() {
+    $("#flame").draggable({
+      
+    });
+    $(".flame").hide();
+    $("#one").droppable({
+        tolerance: 'touch',
+        drop: function(event, ui) {
+            $("#flameone").show();
 
+        }
+    });
+    $("#two").droppable({
+        tolerance: 'touch',
+        drop: function(event, ui) {
+            $("#flametwo").show();
 
+        }
+    });
+    $("#three").droppable({
+        tolerance: 'touch',
+        drop: function(event, ui) {
+            $("#flamethree").show();
 
+        }
+    });
+    $("#four").droppable({
+        tolerance: 'touch',
+        drop: function(event, ui) {
+            $("#flamefour").show();
 
-      $("#droppable").droppable({
-          tolerance: 'touch',
-          drop: function() {
-              $("#droppable")
-                  .addClass("ui-state-highlight")
-                  .find("p")
-                  .html("Dropped with a touch!");
-          }
-      });
-  });
+        }
+    });
+    $("#five").droppable({
+        tolerance: 'touch',
+        drop: function(event, ui) {
+            $("#flamefive").show();
+
+        }
+    });
+    $("#six").droppable({
+        tolerance: 'touch',
+        drop: function(event, ui) {
+            $("#flamesix").show();
+
+        }
+    });
+    $("#seven").droppable({
+        tolerance: 'touch',
+        drop: function(event, ui) {
+            $("#flameseven").show();
+
+        }
+    });
+    $("#eight").droppable({
+        tolerance: 'touch',
+        drop: function(event, ui) {
+            $("#flameeight").show();
+
+        }
+    });
+});
